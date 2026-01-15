ef14eeaed3565c80abffe608ad97d7c398789304
Marcos Cruz <limbolik04@gmail.com>
2026-01-15 11:26:48 -0500

    First arena generation script and scene

--- scripts/arena/arena_generator.gd
+++ scripts/arena/arena_generator.gd
@@ -1,5 +1,27 @@
 extends Node2D
 
-#TODO: Procedural Generation Logic
+@export var width: int = 20
+@export var height: int = 12
+@export var tile_layer: TileMapLayer
+
+# Tile IDs (These depend on your TileSet setup)
+var floor_atlas_coords = Vector2i(0, 0)
+var wall_atlas_coords = Vector2i(6, 12)
+
 func _ready():
-	print("Arena Generator Initializewd")
+	generate_arena()
+
+func generate_arena():
+	if not tile_layer:
+		print("Error: No TileMapLayer assigned!")
+		return
+		
+	# Clear previous data
+	tile_layer.clear()
+	
+	for x in range(width):
+		for y in range(height):
+			if x == 0 or x == width - 1 or y == 0 or y == height - 1:
+				tile_layer.set_cell(Vector2i(x,y), 0, wall_atlas_coords)
+			else:
+				tile_layer.set_cell(Vector2i(x,y), 0, floor_atlas_coords)
---
+++ scripts/player.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+@export var speed = 200
+
+func _physics_process(_delta):
+	# Get input direction (WASD or Arrow keys)
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	# Apply movement
+	velocity = direction * speed
+	move_and_slide()
